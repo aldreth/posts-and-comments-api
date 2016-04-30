6d4f561ca5a2a6d11662500581c7451e3f56280a
Edward Andrews-Hodgson <edward.andrews-hodgson@sky.uk>
2016-04-30 08:19:00 +0100

    Render model errors correctly for ember

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+
+  private
+
+  def render_model_errors(model)
+    render(
+      json: model,
+      status: :unprocessable_entity,
+      adapter: :json_api,
+      serializer: ActiveModel::Serializer::ErrorSerializer
+    )
+  end
 end
